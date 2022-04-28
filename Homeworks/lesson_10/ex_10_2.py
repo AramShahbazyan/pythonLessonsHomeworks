9b6ca607facda4290b73baca1a4eb7faa011f9a0
Aram Shahbazyan <a.shahbazyan@gmail.com>
2022-04-29 01:02:02 +0400

    ex 10_2 uptade

--- Homeworks/lesson_10/ex_10_2.py
+++ Homeworks/lesson_10/ex_10_2.py
@@ -2,13 +2,32 @@
 # Ֆունկցիան պետք վերադարձնի list որում կլինեն ստացած list ում առկա բոլոր թվերի զույգ առ զույգ կոմբինացիայի
 # ընդհանուր ամենամեծ բաժանարարները։
 
-def my_list(first_list: list):
-    mult_list = []
-    for i in first_list:
-        for j in range(1, i + 1):
-            if i % j == 0:
-                mult_list.append(j)
-    print(mult_list)
+def check_mult_max(num: int):
+    div_list = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            div_list.append(i)
+    return div_list
 
 
-my_list([12, 5, 66, 7, 10])
+def two_nums(num1, num2):
+    max = 1
+    l1 = check_mult_max(num1)
+    l2 = check_mult_max(num2)
+    for i in l1:
+        if i in l2:
+            if i > max:
+                max = i
+    return max
+
+
+def create_max_mult_list(ls: list):
+    result = []
+    for i in range(len(ls)):
+        for j in range(i + 1, len(ls)):
+            element = two_nums(ls[i], ls[j])
+            result.append(element)
+    return result
+
+
+print(create_max_mult_list([12, 56, 66, 98, 10]))
